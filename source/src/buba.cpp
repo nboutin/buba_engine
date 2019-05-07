c55a681d124844f36d31e1fb01aa22067a3a8762
Nicolas Boutin <boutwork@gmail.com>
2019-05-07 17:17:13 +0200

    import transaction from ofx to database

--- source/src/buba.cpp
+++ source/src/buba.cpp
@@ -51,11 +51,11 @@ bool Budget_Battle::import_ofx(const std::string& pathname)
 
                 if(b.first == "BANKACCTFROM")
                 {
-                    auto branch = b.second.get("BRANCH", "");
-                    cout << "branch:" << branch << endl;
-
+                    auto branch   = b.second.get("BRANCH", "");
                     auto branchid = b.second.get("BRANCHID", "");
-                    cout << "branchid:" << branchid << endl;
+
+                    //                    cout << "branch:" << branch << endl;
+                    //                    cout << "branchid:" << branchid << endl;
 
                     m_dbp->insert_bank(stoi(branchid));
 
@@ -66,6 +66,34 @@ bool Budget_Battle::import_ofx(const std::string& pathname)
                     for(auto c : b.second)
                     {
                         cout << "c:" << c.first << ":" << c.second.data() << endl;
+
+                        string fitid, date, description;
+                        double amount = 0;
+
+                        if(c.first == "STMTTRN")
+                        {
+                            for(auto d : c.second)
+                            {
+                                if(d.first == "FITID")
+                                {
+                                    fitid = d.second.data();
+                                }
+                                else if(d.first == "DTPOSTED")
+                                {
+                                    date = d.second.data();
+                                }
+                                else if(d.first == "TRNAMT")
+                                {
+                                    amount = strtod(d.second.data().c_str(), nullptr);
+                                }
+                                // Hopefully NAME is last in STMTTRN structure
+                                else if(d.first == "NAME")
+                                {
+                                    description = d.second.data();
+                                    m_dbp->insert_transaction(fitid, date, description, amount);
+                                }
+                            }
+                        }
                     }
                 }
             }
