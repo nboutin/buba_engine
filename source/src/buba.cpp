580c087fd7fd203691e5e2fc126977be13147e50
Nicolas Boutin <boutwork@gmail.com>
2019-05-09 16:11:34 +0200

    add protection on not open database access

--- source/src/buba.cpp
+++ source/src/buba.cpp
@@ -40,9 +40,18 @@ bool Budget_Battle::import_ofx(const std::string& pathname)
     return true;
 }
 
-std::vector<Bank_t> Budget_Battle::get_banks() const { return m_dbp->get_banks(); }
+std::vector<Bank_t> Budget_Battle::get_banks() const
+{
+    if(!m_dbp)
+        return {};
+
+    return m_dbp->get_banks();
+}
 
 std::vector<Transaction_t> Budget_Battle::get_transactions() const
 {
+    if(!m_dbp)
+        return {};
+
     return m_dbp->get_transactions();
 }
